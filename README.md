b3960eb648e925f44fbf464ae232d12b96d5da39
Kessi Diones <kessis01@gmail.com>
2021-01-17 14:41:36 -0300

    Inital Commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Script para envio de e-mails informativos com python.
+Script em python para envio de e-mails.
+
+Script simples que pode ser integrado em servidores Linux, com suporte aos pacotes do Python.
+
+Usei este script juntamente com o Cron do Linux para informar que as rotinas de backup do servidor tinham sido realizadas.
+
+No entando, não existe limites para a aplicação desse script, pode integrado a outros, para criar processos mais elaborados de envios de e-mails em serviços automatizados, até mesmo com ferramentas DevOps como o Ansible.
+
+Estou usando o módulo smtplib do python para estabelecer sessões SMTP cliente.
+
+Para mais informações sobre esse módulo consulte a documentação disponível no site do python.org:
+Link de acesso: https://docs.python.org/3/library/smtplib.html
---
+++ sent_mail_smtp.py
@@ -0,0 +1,45 @@
+##############################################################################
+# Script aviso de Backup realizado
+# Idealizado por Robert Silva e adaptado por Kessi Diones (@kessix) 
+# ################################################################################################
+
+import smtplib
+import os
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from datetime import datetime
+
+# Credenciais da conta de e-mail e servidor
+email_from = 'Conta de E-mail'
+email_password = 'Senha E-mail'
+email_smtp_server = 'FQDN do Servidor SMTP'
+
+# Pegando o horário so sistema
+data = datetime.now()
+data_string = data.strftime("%H:%M:%S - %d/%m/%Y") # formato da data e hora
+print(data_string)
+
+# Corpo do e-mail
+destination = ['Conta de E-mail de Destino'] # e-mail de destino
+subject = 'Título do E-mail'  # Assunto do e-mail
+msg = MIMEMultipart() 
+msg['From'] = email_from
+msg['Subject'] = subject
+text = "Mensagem do E-mail"
+print(text)
+msg_text = MIMEText(text, 'html')
+msg.attach(msg_text)
+
+# Configuração do servidor smtp
+try:
+    smtp = smtplib.SMTP(email_smtp_server, 587)
+    smtp.ehlo()
+    #smtp.starttls() - Usar se servidor tiver conexão TLS
+    smtp.login(email_from, email_password)
+    smtp.sendmail(email_from, ','.join(destination), msg.as_string())
+    smtp.quit()
+    print('Email enviado com sucesso')
+except Exception as err:
+    print(f'Falha ao enviar e-mail: {err}')
+   
